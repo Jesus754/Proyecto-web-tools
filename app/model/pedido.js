4ab47c3b8904e04e0112f963988acbc785ed25ee
= <sirijesus754@gmail.com>
2020-05-30 22:50:08 -0300

    Fixed proyecto

---
+++ app/model/pedido.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var pedidoSchema = new Schema({
+    numero: Number,
+    usuario: [{id: usuario.id, nombre: usuario.nombre, apellido: usuario.apellido, direccion: String}],
+    pedido : [pedidoSchema],
+})
+
+
+module.exports = mongoose.model('pedido', pedidoSchema);
